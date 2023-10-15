dd1ea2253367440f7f0d3ef064e244ba3b2f0125
Mika <mpeersha@ucsc.edu>
2023-10-15 15:46:41 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,25 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Sticker Sketchpad";
 
+// Title
 document.title = gameName;
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+// Canvas
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.width = 256;
+canvas.height = 256;
+const ctx = canvas.getContext("2d");
+if (ctx) {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 app.append(header);
+app.append(canvas);
+
+/*
+Credits:
+- Explained how to use "as HTMLCanvasElement" to not have a problem with the possability of a null canvas
+    https://kernhanda.github.io/tutorial-typescript-canvas-drawing/
+*/
