40b868bcf35677e00546c8ff809619ade5d87403
Mika <mpeersha@ucsc.edu>
2023-10-15 22:52:11 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,16 @@ const advance = 1;
 // Clear canvas
 function clearCanvas() {
   ctx.clearRect(begPoint, begPoint, canvas.width, canvas.height);
-  mousePoints = [];
+  undoList = [];
   isDrawing = false;
+}
+
+// Undo or Redo based on the values of l1 and l2
+function undoRedo(l1: Point[][], l2: Point[][]) {
+  if (l1.length > begPoint) {
+    l2.push(l1.pop()!);
+    canvas.dispatchEvent(drawLine);
+  }
 }
 
 // Title
@@ -27,6 +35,14 @@ const row2 = document.createElement("div");
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "Clear";
 clearButton.addEventListener("click", () => clearCanvas(), false);
+// Undo button
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "Undo";
+undoButton.addEventListener("click", () => undoRedo(undoList, redoList), false);
+// Redo button
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "Redo";
+redoButton.addEventListener("click", () => undoRedo(redoList, undoList), false);
 
 // Canvas
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -35,7 +51,7 @@ canvas.height = 256;
 const ctx = canvas.getContext("2d")!;
 if (ctx) {
   ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(begPoint, begPoint, canvas.width, canvas.height);
 }
 
 // Event + Drawing
@@ -43,13 +59,15 @@ interface Point {
   x: number;
   y: number;
 }
-let mousePoints: Point[][] = [];
+let undoList: Point[][] = [];
+const redoList: Point[][] = [];
 let linePoints: Point[];
 let isDrawing = false;
 const drawLine = new Event("drawing-changed");
 canvas.addEventListener("drawing-changed", () => {
+  ctx.clearRect(begPoint, begPoint, canvas.width, canvas.height);
   // Go thrugh all of the lines
-  for (const line of mousePoints) {
+  for (const line of undoList) {
     ctx.beginPath();
     // Go through all the points in the line and connect them with a path
     for (let i = begPoint; i < line.length; i += advance) {
@@ -67,7 +85,7 @@ canvas.addEventListener("drawing-changed", () => {
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
   linePoints = [];
-  mousePoints.push(linePoints);
+  undoList.push(linePoints);
   linePoints.push({ x: e.offsetX, y: e.offsetY });
 });
 
@@ -85,6 +103,8 @@ canvas.addEventListener("mouseup", () => {
 
 row1.appendChild(canvas);
 row2.appendChild(clearButton);
+row2.appendChild(undoButton);
+row2.appendChild(redoButton);
 board.appendChild(row1);
 board.appendChild(row2);
 app.append(header);
